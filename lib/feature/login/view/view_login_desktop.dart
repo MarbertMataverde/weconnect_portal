e3759cd0819c1a65a38ce828da1217e7986d8d50
Mar Bert John Mataverde <Marbertmataverde.dev@gmail.com>
2022-07-30 16:02:45 +0800

    improve: login desktop responsiveness

--- lib/feature/login/view/view_login_desktop.dart
+++ lib/feature/login/view/view_login_desktop.dart
@@ -4,6 +4,7 @@ import 'package:lottie/lottie.dart';
 import 'package:weconnect_portal/feature/login/widget/widget_login_textbutton.dart';
 import 'package:weconnect_portal/feature/login/widget/widget_login_textformfield.dart';
 import 'package:weconnect_portal/feature/login/widget/widget_svg.dart';
+import 'package:weconnect_portal/global/widget/widget_global_sizedbox.dart';
 import 'package:weconnect_portal/global/widget/widget_global_text.dart';
 import 'package:weconnect_portal/global/widget/widget_global_textbutton.dart';
 
@@ -26,95 +27,113 @@ class _LoginDesktopState extends State<LoginDesktop> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        children: [
-          Expanded(
-            flex: 1,
-            child: Center(
-              child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    svgAssetLogo(
-                      assetPath: 'assets/app_icon/plain_light_logo.svg',
-                      width: MediaQuery.of(context).size.width * 0.080,
-                    ),
-                    SizedBox(
-                      width: 450,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          globalText(
-                            text: 'Login your account.',
-                            textScaleFactor: 3,
-                            fontWeight: FontWeight.w600,
-                          ),
-                          globalText(
-                            text: '"Nurturing Tommorow\'s Noblest"',
-                            textScaleFactor: 1.2,
-                            fontWeight: FontWeight.w100,
-                          ),
-                          loginTextFormField(
-                            context: context,
-                            label: 'Email',
-                            textInputType: TextInputType.emailAddress,
-                            textScaleFactor: 1.2,
-                            prefixIcon: Icon(
-                              Iconsax.sms,
-                              color:
-                                  Theme.of(context).textTheme.bodyMedium!.color,
-                            ),
-                          ),
-                          loginTextFormField(
-                            context: context,
-                            label: 'Password',
-                            textInputType: TextInputType.visiblePassword,
-                            textScaleFactor: 1.2,
-                            isObscure: !_passwordVisible,
-                            prefixIcon: Icon(
-                              Iconsax.password_check,
-                              color:
-                                  Theme.of(context).textTheme.bodyMedium!.color,
-                            ),
-                            passwordVisibilityIconButton: IconButton(
-                              splashRadius: 0.1,
-                              color:
-                                  Theme.of(context).textTheme.bodyMedium!.color,
-                              onPressed: () => setState(
-                                () => _passwordVisible = !_passwordVisible,
-                              ),
-                              icon: Icon(
-                                _passwordVisible
-                                    ? Iconsax.eye
-                                    : Iconsax.eye_slash,
-                              ),
-                            ),
-                          ),
-                          loginForgotPassword(
-                            context: context,
-                          ),
-                          globalTextButton(
-                            context: context,
-                            text: 'Login',
-                            textScaleFactor: 1.5,
-                          ),
-                        ],
+      body: Padding(
+        padding: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width * 0.0500),
+        child: Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Center(
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      svgAssetLogo(
+                        assetPath: 'assets/app_icon/plain_light_logo.svg',
+                        width: 100,
                       ),
-                    ),
-                    loginCreate(context: context),
-                  ],
+                      sizedBox(),
+                      ConstrainedBox(
+                        constraints: const BoxConstraints(
+                          minWidth: 450,
+                          maxWidth: 500,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            globalText(
+                              text: 'Login your account.',
+                              textScaleFactor: 3,
+                              fontWeight: FontWeight.w600,
+                            ),
+                            globalText(
+                              text: '"Nurturing Tommorow\'s Noblest"',
+                              textScaleFactor: 1.2,
+                              fontWeight: FontWeight.w100,
+                            ),
+                            sizedBox(),
+                            loginTextFormField(
+                              context: context,
+                              label: 'Email',
+                              textInputType: TextInputType.emailAddress,
+                              textScaleFactor: 1.2,
+                              prefixIcon: Icon(
+                                Iconsax.sms,
+                                color: Theme.of(context)
+                                    .textTheme
+                                    .bodyMedium!
+                                    .color,
+                              ),
+                            ),
+                            sizedBox(),
+                            loginTextFormField(
+                              context: context,
+                              label: 'Password',
+                              textInputType: TextInputType.visiblePassword,
+                              textScaleFactor: 1.2,
+                              isObscure: !_passwordVisible,
+                              prefixIcon: Icon(
+                                Iconsax.password_check,
+                                color: Theme.of(context)
+                                    .textTheme
+                                    .bodyMedium!
+                                    .color,
+                              ),
+                              passwordVisibilityIconButton: IconButton(
+                                splashRadius: 0.1,
+                                color: Theme.of(context)
+                                    .textTheme
+                                    .bodyMedium!
+                                    .color,
+                                onPressed: () => setState(
+                                  () => _passwordVisible = !_passwordVisible,
+                                ),
+                                icon: Icon(
+                                  _passwordVisible
+                                      ? Iconsax.eye
+                                      : Iconsax.eye_slash,
+                                ),
+                              ),
+                            ),
+                            loginForgotPassword(
+                              context: context,
+                            ),
+                            sizedBox(),
+                            globalTextButton(
+                              context: context,
+                              text: 'Login',
+                              textScaleFactor: 1.5,
+                            ),
+                          ],
+                        ),
+                      ),
+                      sizedBox(),
+                      loginCreate(context: context),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Lottie.network(
-              'https://assets10.lottiefiles.com/packages/lf20_1pxqjqps.json',
+            Expanded(
+              flex: 1,
+              child: Lottie.network(
+                'https://assets10.lottiefiles.com/packages/lf20_1pxqjqps.json',
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
