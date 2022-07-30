965cf4bfea69e8d352c06573fcfbf05c278b02c2
Mar Bert John Mataverde <Marbertmataverde.dev@gmail.com>
2022-07-30 15:13:06 +0800

    dev: device preview

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,6 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:weconnect_portal/feature/login/view/view_login.dart';
 import 'package:weconnect_portal/firebase_options.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:weconnect_portal/theme.dart';
 
 void main() async {
   /// Firebase Initialization
@@ -8,21 +12,25 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(const BaseCommit());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => const App(), // Wrap your app
+    ),
+  );
 }
 
-class BaseCommit extends StatelessWidget {
-  const BaseCommit({Key? key}) : super(key: key);
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Base Commit',
-      home: Scaffold(
-        body: Center(
-          child: Text('Base Commit'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: lightThemeData,
+      themeMode: ThemeMode.dark,
+      darkTheme: darkThemeData,
+      home: const Login(),
     );
   }
 }
