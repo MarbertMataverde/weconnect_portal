67923c6d67cfa50a74cb99076b6f4d36287ad893
Mar Bert John Mataverde <Marbertmataverde.dev@gmail.com>
2022-07-30 16:37:00 +0800

    improve: forgot password phone responsiveness

--- lib/feature/forgot_password/view/view_forgot_password_phone.dart
+++ lib/feature/forgot_password/view/view_forgot_password_phone.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:weconnect_portal/global/widget/widget_global_appbar.dart';
+import 'package:weconnect_portal/global/widget/widget_global_sizedbox.dart';
 import 'package:weconnect_portal/global/widget/widget_global_text.dart';
 import 'package:weconnect_portal/global/widget/widget_global_textbutton.dart';
 import 'package:weconnect_portal/global/widget/widget_global_textformfield.dart';
@@ -10,39 +12,40 @@ class ForgotPasswordPhone extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: globalAppBar(
+        context: context,
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
       body: Padding(
         padding: EdgeInsets.symmetric(
-            horizontal: MediaQuery.of(context).size.width * 0.1),
-        child: Center(
-          child: SizedBox(
-            width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                globalText(text: 'Forgot your password?', textScaleFactor: 2),
-                globalText(
-                  text:
-                      'Enter your registered email below\nto receive password reset instruction',
-                  textAlign: TextAlign.center,
-                  fontWeight: FontWeight.w100,
-                ),
-                globalTextFormField(
-                  context: context,
-                  hint: 'Email Address',
-                  textInputType: TextInputType.emailAddress,
-                  prefixIcon: Icon(
-                    Iconsax.sms,
-                    color: Theme.of(context).textTheme.bodyMedium!.color,
-                  ),
-                ),
-                globalTextButton(
-                  context: context,
-                  text: 'Send',
-                )
-              ],
+            horizontal: MediaQuery.of(context).size.width * 0.020),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            globalText(text: 'Forgot your password?', textScaleFactor: 1.5),
+            globalText(
+              text:
+                  'Enter your registered email below\nto receive password reset instruction',
+              fontWeight: FontWeight.w100,
             ),
-          ),
+            sizedBox(),
+            globalTextFormField(
+              context: context,
+              hint: 'Email Address',
+              textInputType: TextInputType.emailAddress,
+              prefixIcon: Icon(
+                Iconsax.sms,
+                color: Theme.of(context).textTheme.bodyMedium!.color,
+              ),
+            ),
+            sizedBox(),
+            globalTextButton(
+              context: context,
+              text: 'Send',
+            )
+          ],
         ),
       ),
     );
