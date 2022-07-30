6f6f2470f24d04810f944463deea5590da720989
Mar Bert John Mataverde <Marbertmataverde.dev@gmail.com>
2022-07-30 14:40:12 +0800

    feature: login
    Added login feature with packages:
    - Iconsax
    - Lottie
    - Flutter SVG

---
+++ lib/feature/login/view/veiw.dart
@@ -0,0 +1,3 @@
+export 'view_login_phone.dart';
+export 'view_login_tablet.dart';
+export 'view_login_desktop.dart';
---
+++ lib/feature/login/widget/widget_login_textbutton.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:weconnect_portal/global/widget/widget_global_text.dart';
+
+Widget loginForgotPassword({
+  required BuildContext context,
+}) {
+  return Align(
+    alignment: Alignment.centerRight,
+    child: TextButton(
+      onPressed: () => Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (routeContext) => const ForgotPassword(),
+        ),
+      ),
+      style: TextButton.styleFrom(
+        primary: Theme.of(context).primaryColor,
+      ),
+      child: globalText(
+        text: 'Reset Password',
+        fontWeight: FontWeight.w300,
+      ),
+    ),
+  );
+}
+
+Widget loginCreate({required BuildContext context}) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      globalText(
+        text: 'Dont have an account?',
+        fontWeight: FontWeight.w300,
+      ),
+      TextButton(
+        onPressed: () {},
+        child: globalText(
+          text: 'Create',
+          color: Theme.of(context).primaryColor,
+          fontWeight: FontWeight.w300,
+        ),
+      ),
+    ],
+  );
+}
