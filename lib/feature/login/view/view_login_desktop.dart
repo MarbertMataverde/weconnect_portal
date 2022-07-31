f68d0d7e61b2a14a252dbe341ed10f88ddc22ee4
Mar Bert John Mataverde <Marbertmataverde.dev@gmail.com>
2022-07-31 16:32:28 +0800

    feature: authentication login desktop

--- lib/feature/login/view/view_login_desktop.dart
+++ lib/feature/login/view/view_login_desktop.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:lottie/lottie.dart';
+import 'package:weconnect_portal/feature/authentication/model/model_sign_in.dart';
 import 'package:weconnect_portal/feature/login/widget/widget_login_textbutton.dart';
 import 'package:weconnect_portal/feature/login/widget/widget_login_textformfield.dart';
 import 'package:weconnect_portal/feature/login/widget/widget_svg.dart';
@@ -18,10 +19,24 @@ class LoginDesktop extends StatefulWidget {
 }
 
 class _LoginDesktopState extends State<LoginDesktop> {
+  final _validationKey = GlobalKey<FormState>();
+
+  late TextEditingController emailTextController;
+  late TextEditingController passwordTextController;
+
   @override
   void initState() {
     super.initState();
+    emailTextController = TextEditingController();
+    passwordTextController = TextEditingController();
     _passwordVisible = false;
+  }
+
+  @override
+  void dispose() {
+    emailTextController.dispose();
+    passwordTextController.dispose();
+    super.dispose();
   }
 
   @override
@@ -64,49 +79,78 @@ class _LoginDesktopState extends State<LoginDesktop> {
                               fontWeight: FontWeight.w100,
                             ),
                             sizedBox(),
-                            loginTextFormField(
-                              context: context,
-                              label: 'Email',
-                              textInputType: TextInputType.emailAddress,
-                              textScaleFactor: 1.2,
-                              prefixIcon: Icon(
-                                Iconsax.sms,
-                                color: Theme.of(context)
-                                    .textTheme
-                                    .bodyMedium!
-                                    .color,
+                            Form(
+                              key: _validationKey,
+                              child: Column(
+                                children: [
+                                  loginTextFormField(
+                                    controller: emailTextController,
+                                    context: context,
+                                    label: 'Email',
+                                    textInputType: TextInputType.emailAddress,
+                                    textScaleFactor: 1.2,
+                                    prefixIcon: Icon(
+                                      Iconsax.sms,
+                                      color: Theme.of(context)
+                                          .textTheme
+                                          .bodyMedium!
+                                          .color,
+                                    ),
+                                    validator: (value) {
+                                      bool isEmailValid = RegExp(
+                                              r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                          .hasMatch(value!);
+                                      if (value.isEmpty) {
+                                        return 'Please enter email';
+                                      }
+                                      if (!isEmailValid) {
+                                        return 'Invalid email';
+                                      }
+                                      return null;
+                                    },
+                                  ),
+                                  loginTextFormField(
+                                    controller: passwordTextController,
+                                    context: context,
+                                    label: 'Password',
+                                    textInputType:
+                                        TextInputType.visiblePassword,
+                                    textScaleFactor: 1.2,
+                                    isObscure: !_passwordVisible,
+                                    prefixIcon: Icon(
+                                      Iconsax.password_check,
+                                      color: Theme.of(context)
+                                          .textTheme
+                                          .bodyMedium!
+                                          .color,
+                                    ),
+                                    passwordVisibilityIconButton: IconButton(
+                                      splashRadius: 0.1,
+                                      color: Theme.of(context)
+                                          .textTheme
+                                          .bodyMedium!
+                                          .color,
+                                      onPressed: () => setState(
+                                        () => _passwordVisible =
+                                            !_passwordVisible,
+                                      ),
+                                      icon: Icon(
+                                        _passwordVisible
+                                            ? Iconsax.eye
+                                            : Iconsax.eye_slash,
+                                      ),
+                                    ),
+                                    validator: (value) {
+                                      if (value!.isEmpty) {
+                                        return 'Please enter password';
+                                      }
+                                      return null;
+                                    },
+                                  ),
+                                ],
                               ),
                             ),
                             sizedBox(),
-                            loginTextFormField(
-                              context: context,
-                              label: 'Password',
-                              textInputType: TextInputType.visiblePassword,
-                              textScaleFactor: 1.2,
-                              isObscure: !_passwordVisible,
-                              prefixIcon: Icon(
-                                Iconsax.password_check,
-                                color: Theme.of(context)
-                                    .textTheme
-                                    .bodyMedium!
-                                    .color,
-                              ),
-                              passwordVisibilityIconButton: IconButton(
-                                splashRadius: 0.1,
-                                color: Theme.of(context)
-                                    .textTheme
-                                    .bodyMedium!
-                                    .color,
-                                onPressed: () => setState(
-                                  () => _passwordVisible = !_passwordVisible,
-                                ),
-                                icon: Icon(
-                                  _passwordVisible
-                                      ? Iconsax.eye
-                                      : Iconsax.eye_slash,
-                                ),
-                              ),
-                            ),
                             loginForgotPassword(
                               context: context,
                             ),
@@ -115,12 +159,26 @@ class _LoginDesktopState extends State<LoginDesktop> {
                               context: context,
                               text: 'Login',
                               textScaleFactor: 1.5,
+                              onPressed: () {
+                                FocusScope.of(context).unfocus();
+                                if (_validationKey.currentState!.validate() ==
+                                    true) {
+                                  signInWithEmailAndPassword(
+                                    context: context,
+                                    email: emailTextController.text.trim(),
+                                    password:
+                                        passwordTextController.text.trim(),
+                                  );
+                                }
+                              },
                             ),
                           ],
                         ),
                       ),
                       sizedBox(),
-                      loginCreate(context: context),
+                      loginCreate(
+                        context: context,
+                      ),
                     ],
                   ),
                 ),
