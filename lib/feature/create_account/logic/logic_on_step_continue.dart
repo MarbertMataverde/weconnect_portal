c16b696455443b3e0ea2b8e4a1d02845c387e87d
Mar Bert John Mataverde <Marbertmataverde.dev@gmail.com>
2022-08-02 11:58:51 +0800

    added: logic step function for personal information
    This handles the validation of personal information step when continue is cliked.

---
+++ lib/feature/create_account/logic/logic_on_step_continue.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:weconnect_portal/global/widget/widget_global_dialog.dart';
+
+void Function()? personalInformationStepContinue({
+  required BuildContext context,
+  required int currentStep,
+  required GlobalKey<FormState> personalInformationValidationKey,
+  required String? genderSelectedItem,
+  required DateTime? birthDate,
+  required void Function() nextStep,
+}) {
+  return () {
+    FocusScope.of(context).unfocus();
+    if (personalInformationValidationKey.currentState!.validate() == true) {
+      if (genderSelectedItem == null && birthDate == null) {
+        globalDialog(
+          context: context,
+          title: 'Empty Field',
+          content: 'Gender & Birthdate can\'t be empty',
+        );
+      } else if (genderSelectedItem == null) {
+        globalDialog(
+          context: context,
+          title: 'Gender Missing',
+          content: 'Gender can\'t be empty',
+        );
+      } else if (birthDate == null) {
+        globalDialog(
+          context: context,
+          title: 'Birthdate Missing',
+          content: 'Birthdate can\'t be empty',
+        );
+      } else {
+        nextStep();
+      }
+    }
+  };
+}
