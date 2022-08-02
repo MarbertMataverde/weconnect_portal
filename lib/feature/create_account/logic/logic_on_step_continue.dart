11ce40623b70542e3ebfda60f7b734af9942f2ab
Mar Bert John Mataverde <Marbertmataverde.dev@gmail.com>
2022-08-02 15:49:33 +0800

    added: logic account information step

--- lib/feature/create_account/logic/logic_on_step_continue.dart
+++ lib/feature/create_account/logic/logic_on_step_continue.dart
@@ -16,23 +16,61 @@ void Function()? personalInformationStepContinue({
         globalDialog(
           context: context,
           title: 'Empty Field',
-          content: 'Gender & Birthdate can\'t be empty',
+          content: 'Gender & Birthdate can\'t be empty.',
         );
       } else if (genderSelectedItem == null) {
         globalDialog(
           context: context,
           title: 'Gender Missing',
-          content: 'Gender can\'t be empty',
+          content: 'Gender can\'t be empty.',
         );
       } else if (birthDate == null) {
         globalDialog(
           context: context,
           title: 'Birthdate Missing',
-          content: 'Birthdate can\'t be empty',
+          content: 'Birthdate can\'t be empty.',
         );
       } else {
         nextStep();
       }
+    }
+  };
+}
+
+void Function()? accountInformationStepContinue({
+  required BuildContext context,
+  required String? accountTypeSelectedItem,
+  required GlobalKey<FormState> accountInformationValidationKey,
+  required String? selectedCollege,
+  required void Function() nextStep,
+}) {
+  return () {
+    FocusScope.of(context).unfocus();
+    if (accountTypeSelectedItem != null &&
+        accountTypeSelectedItem == 'Student') {
+      if (accountInformationValidationKey.currentState!.validate() == true) {
+        if (selectedCollege == null) {
+          globalDialog(
+            context: context,
+            title: 'College',
+            content: 'College can\'t be empty',
+          );
+        } else {
+          nextStep();
+        }
+      }
+    } else if (accountTypeSelectedItem == 'Professor') {
+      if (accountInformationValidationKey.currentState!.validate() == true) {
+        nextStep();
+      }
+    } else if (accountTypeSelectedItem == null) {
+      globalDialog(
+        context: context,
+        title: 'Account Type',
+        content: 'Please select account type for this account.',
+      );
+    } else {
+      nextStep();
     }
   };
 }
