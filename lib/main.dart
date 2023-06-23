607a7bf7c8abf9ae91d14a95bcd363837098bbe6
Mar Bert John Mataverde <94389554+MarbertMataverde@users.noreply.github.com>
2023-06-23 22:02:16 +0800

    docs: Add code documentation to main.dart
    - Added comprehensive code documentation to the
    main.dart file.
    
    - Documented the purpose of the file, which serves as the entry point of the application.
    - Described the functionalities including Firebase initialization, theme setup, and root widget definition.
    
    - Documented the main() function and its responsibility of initializing Firebase and running the app.
    - Documented the App widget and its role as the root widget of the application.
    
    - Provided documentation for the properties and logic within the App widget.
    
    - This commit enhances code readability and maintainability by providing detailed explanations for the code sections.
    
    This commit reflects a docs type, as it focuses on adding documentation to the codebase without changing its functionality.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,9 @@
+/// File: main.dart
+///
+/// Description: This file serves as the entry point of the application.
+/// It initializes Firebase, sets up the application's theme, and defines the root widget.
+///
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -5,6 +11,8 @@ import 'package:weconnect_portal/features/login/view/view_login.dart';
 import 'package:weconnect_portal/firebase_options.dart';
 import 'package:weconnect_portal/theme.dart';
 
+/// The main function that initializes the Flutter application.
+/// It ensures Firebase is initialized and then runs the app.
 Future<void> main() async {
   /// Firebase Initialization
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,6 +23,8 @@ Future<void> main() async {
   runApp(const App());
 }
 
+/// The root widget of the application.
+/// It sets up the MaterialApp and defines the theme and initial route.
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
@@ -22,20 +32,28 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+
+      /// Set the light theme as the default theme for the application.
       theme: lightThemeData,
+
+      /// Set the dark theme as the default dark theme for the application.
       themeMode: ThemeMode.dark,
       darkTheme: darkThemeData,
+
+      /// Determine the initial screen based on the user's authentication state.
+      /// If the user is authenticated, show the home screen.
+      /// If the user is not authenticated, show the login screen.
       home: StreamBuilder<User?>(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return Container(); // loading
+            return Container(); // Show a loading indicator.
           } else if (snapshot.hasError) {
-            return Container(); // restart the app message
+            return Container(); // Show a message to restart the app.
           } else if (snapshot.hasData) {
-            return Container(); // home screen
+            return Container(); // Show the home screen.
           } else {
-            return const Login();
+            return const Login(); // Show the login screen.
           }
         },
       ),
